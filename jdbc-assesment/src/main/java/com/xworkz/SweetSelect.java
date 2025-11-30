67a43c44aebcc2b964a9b26330ebf8ff91f2bf0e
SannidhiGS <ssannidhigs@gmail.com>
2025-11-30 22:59:54 +0530

    adding jdbc

---
+++ jdbc-assesment/src/main/java/com/xworkz/SweetSelect.java
@@ -0,0 +1,32 @@
+class Wire{
+	
+	int wireId;
+	String brand;
+	int wireLen;
+	String color;
+	double price;
+	
+	Wire(int wireId){
+		this.wireId;	
+	}
+	Wire(String brand){
+		this.brand=brand;
+	}
+	Wire(double price){
+		this.price=price;
+	}
+	Wire(int wireId,String brand){
+		this.wireId=wireId;
+		this.brand=brand;	
+	}
+	Wire(int wireId,String brand,int wireLen){
+		this.wireId=wireId;
+		this.brand=brand;
+	
+	}
+	
+
+
+
+
+}
