0172156190e6616470c8db1d6b119504e6b00f9a
SannidhiGS <ssannidhigs@gmail.com>
2025-12-10 07:35:01 +0530

    adding fuel station

--- cattle-farming/src/main/java/com/xworkz/cattle/exeception/DataInvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.cattle.exeception;
-
-public class DataInvalidException extends Exception{
-    public DataInvalidException(String msg){
-        super(msg);
-    }
-}
--- cattle-farming/src/main/java/com/xworkz/cattle/exeception/DuplicateException.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.cattle.exeception;
-
-public class DuplicateException extends Exception {
-    public DuplicateException(String msg){
-        super(msg);
-    }
-}
